a8a8b3b71aafb409f8c5fad822af95a7168a6394
Arsalan <arsalank.ansari@gmail.com>
2023-07-24 22:05:38 -0400

    finaly done with the server.js and added a readme

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const { v4: uuidv4 } = require("uuid");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.post("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    var notes = JSON.parse(data);
+    let userNote = req.body;
+    userNote.id = uuidv4();
+    notes.push(userNote);
+    fs.writeFile("./db/db.json", JSON.stringify(notes), (err, data) => {
+      res.json(userNote);
+    });
+  });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    let notes = JSON.parse(data);
+    const newNotes = notes.filter((note) => note.id !== req.params.id);
+
+    fs.writeFile("./db/db.json", JSON.stringify(newNotes), (err, data) => {
+      res.json({ msg: "successful" });
+    });
+  });
+});
+
+app.get("api/notes/:id", (req, res) => {
+  res.json(notes[req.params.id]);
+});
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    var notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT} 🚀`);
+});
